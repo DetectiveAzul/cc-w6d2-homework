d843c326f4ad7ab5f22f9d1af04c299acfad9041
Jaime Lopez <detec.azul@gmail.com>
2018-04-10 18:54:49 +0100

    Changed way HASH stock is updated

--- start_point/src/main/java/Library.java
+++ start_point/src/main/java/Library.java
@@ -10,15 +10,34 @@ public class Library {
         this.capacity = capacity;
         this.books = new ArrayList<>();
         this.stock = new HashMap<>();
+        for (Genre key:
+             Genre.values()) {
+            stock.put(key, 0);
+        }
     }
 
     public int bookCount() {
         return books.size();
     }
 
+    public void addStock(Book book) {
+        Genre genre = book.getGenre();
+        int value = stock.get(genre);
+        value++;
+        stock.replace(genre,value);
+    }
+
+    public void removeStock(Book book) {
+        Genre genre = book.getGenre();
+        int value = stock.get(genre);
+        value--;
+        stock.replace(genre,value);
+    }
+
     public void addBook(Book book) {
        if (!isFull()) {
            this.books.add(book);
+           addStock(book);
        }
 
     }
@@ -28,22 +47,23 @@ public class Library {
     }
 
     public Book sellBook() {
+        removeStock(books.get(0));
         return books.remove(0);
     }
 
-    private void updateStock(Genre genre) {
-        int total = 0;
-        for (Book book: books) {
-            Genre book_genre = book.getGenre();
-            if (book_genre == genre) {
-                total++;
-            }
-        }
-        stock.put(genre, total);
-    }
-
+    //Added individual methods to add and remove stock, not needed
+//    private void updateStock(Genre genre) {
+//        int total = 0;
+//        for (Book book: books) {
+//            Genre book_genre = book.getGenre();
+//            if (book_genre == genre) {
+//                total++;
+//            }
+//        }
+//        stock.put(genre, total);
+//    }
+//
     public int getStock(Genre genre) {
-        updateStock(genre);
         return stock.get(genre);
     }
 }
